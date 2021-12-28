0afe55efcdf17d11359c024534274326dc602717
Alex Chautemps <chautemps.alexandre@gmail.com>
2021-12-28 21:38:32 +0100

    CSS searchBar

--- components/search.js
+++ components/search.js
@@ -5,16 +5,17 @@ export default function Search({ handleChange }) {
         <input
           className='search__input'
           onChange={handleChange}
-          placeholder='Search'
+          placeholder='Chercher un film'
         />
       </div>
       <style jsx>{`
+        @import '../styles/shared';
         .search {
           padding: 0 calc(var(--gutter) * 2);
           &__input {
-            border-radius: 50px;
+            border-radius: rem(8);
             padding: 0.5rem;
-            border: 1px solid black;
+            border: 1px solid $light-grey;
             width: 100%;
           }
         }
