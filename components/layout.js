3520921bed87c4bc0b846df2e6cb13da9972a3fd
Alex Chautemps <chautemps.alexandre@gmail.com>
2021-12-28 19:20:06 +0100

    Footer

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,14 @@
 import Grid from './debug-grid';
+import Footer from './footer';
+
 export default function Layout({ children, show_debug_grid }) {
   return (
-    <main>
-      {show_debug_grid && <Grid />}
-      <div className='o-container'>{children}</div>
-    </main>
+    <>
+      <main>
+        {show_debug_grid && <Grid />}
+        <div className='o-container'>{children}</div>
+      </main>
+      <Footer />
+    </>
   );
 }
