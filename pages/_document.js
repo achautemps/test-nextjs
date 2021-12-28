36afcc1b0831024ef8ebc62749b998268334b35d
Alex Chautemps <chautemps.alexandre@gmail.com>
2021-12-28 23:10:00 +0100

    Fix lighthouse errors

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='fr-fr'>
         <Head>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
@@ -14,6 +14,10 @@ class MyDocument extends Document {
           <link
             href='https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap'
             rel='stylesheet'
+          />
+          <meta
+            name='description'
+            content='Test next.js par Alexandre Chautemps'
           />
         </Head>
         <body>
